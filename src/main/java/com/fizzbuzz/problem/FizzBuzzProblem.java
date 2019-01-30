a1d44bed91c32f1e0502a77a5dab912edccb8dfc
Shivangi Johri <shivangi8293@gmail.com>
2019-01-30 12:22:30 +0530

    Added Failing test case for Output "FizzBuzz"

--- src/main/java/com/fizzbuzz/problem/FizzBuzzProblem.java
+++ src/main/java/com/fizzbuzz/problem/FizzBuzzProblem.java
@@ -1,4 +1,13 @@
 package com.fizzbuzz.problem;
 
 public class FizzBuzzProblem {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String getFizzBuzz(int number){
+
+        return "";
+    }
 }
--- src/test/java/com/fizzbuzz/problem/FizzBuzzProblemTest.java
+++ src/test/java/com/fizzbuzz/problem/FizzBuzzProblemTest.java
@@ -1,10 +1,21 @@
 package com.fizzbuzz.problem;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzProblemTest {
+
+
+    @Test
+    public void testGetFizzBuzz_ForOutputFizzbuzz(){
+        FizzBuzzProblem fizzBuzzProblem= new FizzBuzzProblem();
+        String actual = fizzBuzzProblem.getFizzBuzz(15);
+
+        assertEquals("fizzbuzz" , actual);
+
+    }
 
 
 }
