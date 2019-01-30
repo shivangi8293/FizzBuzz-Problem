d162c4e03127d61742fe5cd73a589751a61eac6b
Shivangi Johri <shivangi8293@gmail.com>
2019-01-30 13:56:20 +0530

    Incorporated Review Comments
     - Change test methods name to make test more readable
     - Simplified code
     - Created setUp method in the test class to remove the redundant code
     - Added more testing scenarios in each test case

--- src/test/java/com/fizzbuzz/problem/FizzBuzzProblemTest.java
+++ src/test/java/com/fizzbuzz/problem/FizzBuzzProblemTest.java
@@ -1,46 +1,47 @@
 package com.fizzbuzz.problem;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzProblemTest {
 
+    FizzBuzzProblem fizzBuzzProblem= null;
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzzProblem= new FizzBuzzProblem();
+    }
 
     @Test
-    public void testGetFizzBuzz_ForOutputFizzbuzz(){
-        FizzBuzzProblem fizzBuzzProblem= new FizzBuzzProblem();
-        String actual = fizzBuzzProblem.getFizzBuzz(15);
+    public void should_return_FizzBuzz_if_number_is_divisible_by_15(){
 
-        assertEquals("fizzbuzz" , actual);
+        assertEquals("fizzbuzz" , fizzBuzzProblem.getFizzBuzz(15));
+        assertEquals("fizzbuzz" , fizzBuzzProblem.getFizzBuzz(45));
 
     }
 
     @Test
-    public void testGetFizzBuzz_ForOutputBuzz(){
-        FizzBuzzProblem fizzBuzzProblem= new FizzBuzzProblem();
-        String actual = fizzBuzzProblem.getFizzBuzz(10);
+    public void should_return_Buzz_if_number_is_divisible_by_5(){
 
-        assertEquals("buzz" , actual);
-
+        assertEquals("buzz" , fizzBuzzProblem.getFizzBuzz(10));
+        assertEquals("buzz" , fizzBuzzProblem.getFizzBuzz(80));
     }
 
 
     @Test
-    public void testGetFizzBuzz_ForOutputFizz(){
-        FizzBuzzProblem fizzBuzzProblem= new FizzBuzzProblem();
-        String actual = fizzBuzzProblem.getFizzBuzz(9);
+    public void should_return_Fizz_if_number_is_divisible_by_3(){
 
-        assertEquals("fizz" , actual);
+        assertEquals("fizz" , fizzBuzzProblem.getFizzBuzz(9));
+        assertEquals("fizz" , fizzBuzzProblem.getFizzBuzz(27));
 
     }
 
     @Test
-    public void testGetFizzBuzz_ForNoMatchingOutput(){
-        FizzBuzzProblem fizzBuzzProblem= new FizzBuzzProblem();
-        String actual = fizzBuzzProblem.getFizzBuzz(17);
+    public void should_return_number_if_number_is_not_divisible_by_3_5_15(){
 
-        assertEquals("17" , actual);
+        assertEquals("17" , fizzBuzzProblem.getFizzBuzz(17));
+        assertEquals("-76" , fizzBuzzProblem.getFizzBuzz(-76));
 
     }
 
